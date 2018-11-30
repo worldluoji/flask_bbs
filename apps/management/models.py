381b333368e35c14e3129cd5c604e7eb7d012523
luoji <qq1132416@outlook.com>
2018-11-30 16:07:31 +0800

    Role Model added

--- apps/management/models.py
+++ apps/management/models.py
@@ -36,3 +36,42 @@ class Administrator(db.Model):
 
     def check_password(self,password):
         return check_password_hash(self.password,password)
+
+
+class UserRights(object):
+    #bitmap 11111111
+    ALLOWED_ALL = 0b11111111
+
+    VISITOR = 0b00000001
+
+    POSTER = 0b00000010
+
+    COMMENTER = 0b00000100
+
+    BOARDEF = 0b00001000
+
+    FRONTUSER = 0b00010000
+
+    ADMINER = 0b00100000
+
+    DEVELOPER = 0b01000000
+
+
+adminer_role = db.Table(
+    'adminer_role_table',
+    db.Column('role_id',db.Integer,db.ForeignKey('role.id'),primary_key=True),
+    db.Column('admin_id',db.Integer,db.ForeignKey('administrator.id'),primary_key=True)
+)
+
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(150), nullable=False)
+    create_time = db.Column(db.DATETIME, default=datetime.now)
+    rights = db.Column(db.Integer,default=UserRights.VISITOR)
+    users = db.relationship('Administrator', secondary=adminer_role, backref="roles")
+
+    def __str__(self):
+        return self.name
--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ Migrate(app,db)
 manager.add_command('db', MigrateCommand)
 
 Adminer = admin_models.Administrator
-
+Role = admin_models.Role
 
 #python3 manage.py create_manager -u 'luoji' -p '199114' -e '1132416@qq.com'
 @manager.option('-u','--username',dest='username')
@@ -28,6 +28,25 @@ def create_manager(username,password,email):
     db.session.add(user)
     db.session.commit()
     print('Add an administrator successfully!!!')
+
+#python3 manage.py crate_role
+@manager.command
+def create_role():
+    visitor = Role(name="visitor",desc="can only visit")
+    visitor.rights = admin_models.UserRights.VISITOR
+
+    operator = Role(name="operator",desc="can manage comment, article or frontuser")
+    operator.rights = (admin_models.UserRights.VISITOR | admin_models.UserRights.COMMENTER | admin_models.UserRights.POSTER | admin_models.UserRights.FRONTUSER)
+
+    adminer = Role(name="adminer",desc="having all rights")
+    adminer.rights = (admin_models.UserRights.VISITOR | admin_models.UserRights.COMMENTER | admin_models.UserRights.POSTER | admin_models.UserRights.FRONTUSER
+                      | admin_models.UserRights.BOARDEF | admin_models.UserRights.ADMINER)
+
+    developer = Role(name="developer", desc="having all rights")
+    developer.rights = admin_models.UserRights.DEVELOPER
+
+    db.session.add_all([visitor,operator,adminer,developer])
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
