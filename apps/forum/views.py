0f0262152b1cde368c40cbd50203b9f32bc5c63d
luoji <qq1132416@outlook.com>
2018-12-05 20:50:13 +0800

    frontuser signup template added

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,9 +1,20 @@
 
 
-from flask import Blueprint
+from flask import Blueprint,views,render_template
 
 bp = Blueprint('forum',__name__,url_prefix='/forum')
 
 @bp.route('/')
 def index():
     return 'forum index'
+
+
+class SignUpView(views.MethodView):
+    def get(self):
+        return render_template('forum/fuser_signup.html')
+
+    def post(self):
+        pass
+
+
+bp.add_url_rule('/signup/',view_func=SignUpView.as_view('signup'))
